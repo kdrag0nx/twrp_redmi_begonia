2e40e80b802635b7c07de8d987bab8267c50ab97
TheMalachite <eliasgheeraert@gmail.com>
2020-11-15 14:03:37 +0600

    begonia: Update Tree To Compatible For Twrp 10.0
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>

--- omni_begonia.mk
+++ omni_begonia.mk
@@ -14,10 +14,9 @@
 # limitations under the License.
 #
 
-# Release name
-PRODUCT_RELEASE_NAME := begonia
-
-$(call inherit-product, build/target/product/embedded.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
